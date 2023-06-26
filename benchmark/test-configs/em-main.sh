4974694c77e555c8c16ad3f1753efe19bd7b9a63
ternava <t.xheva@gmail.com>
2023-06-26 14:40:05 +0200

    dissabled debugging and reverted the changed file of "em-main.sh" by accident

--- benchmark/test-configs/em-main.sh
+++ benchmark/test-configs/em-main.sh
@@ -1,61 +1,87 @@
 #!/usr/bin/env bash
 
-# use it for debugging
-set -x
+#set -x
 
-main() {
-    local program_path="$1"
-    local cp="cp"
-    local source=./test-configs/*
-    local destination="./"
-
-    validate_inputs "$program_path" "$destination"
-
-    perform_cp "$program_path" "$cp" $source "$destination"
+check_arguments() {
+    if [[ $# -ne 1 ]]
+    then
+        # <thecase> presents the specific script with a particular usage of a program
+        echo "Error: Please provide one script as argument."
+        echo "Usage: $0 <thecase>"
+        exit 1
+    fi
 }
 
 validate_inputs() {
-    local program_path="$1"
-    #local source="$2"
-    local destination="$2"
+    local thecase_path="$1"
+    local theprogram_path="$2"
+    local the_juleit="$3"
 
-    if [ ! -e "$program_path" ]
+    if [ -z "$thecase_path" ] || [ ! -f "$thecase_path" ]
     then
-        echo "The program path '$program_path' does not exist."
+        echo "Error: Invalid or missing '$thecase_path' script. Please check the path."
         exit 1
     fi
 
-    #if [ ! -d $source ]
-    #then
-    #    echo "The source directory '$source' does not exist."
-    #    exit 1
-    #fi
-
-    if [ ! -d "$destination" ]
-    then
-        echo "The destination directory '$destination' does not exist."
+    if [ ! -e "$theprogram_path" ] || [ ! -e "$the_juleit" ]; then
+        echo "The program path '$theprogram_path' and/or '$the_juleit' does not exist."
         exit 1
     fi
 }
 
-perform_cp() {
-    local program_path="$1"
-    local cp_command="$2"
-    local source="$3"
-    local destination="$4"
+catch_errors_from_nested_script() {
+    local thecase_path="$1"
+    local theprogram_path="$2"
 
-    # Usage: ./pre-experiment/exe-GNU-v93/cp [OPTION]... [-T] SOURCE DEST
-    #    or:  ./pre-experiment/exe-GNU-v93/cp [OPTION]... SOURCE... DIRECTORY
-    #    or:  ./pre-experiment/exe-GNU-v93/cp [OPTION]... -t DIRECTORY SOURCE...
-    #       Copy SOURCE to DEST, or multiple SOURCE(s) to DIRECTORY.
-    "$program_path/$cp_command" -R $source "$destination" # >/dev/null 2>&1
-    local exit_status=$?
+    # Call the script and redirect stderr to stdout
+    output="$("$thecase_path" "$theprogram_path" 2>&1)"
 
-    if [ $exit_status -ne 0 ]
+    # Check if the script returned an error
+    if [[ $? -ne 0 ]]
     then
-        echo "Error occurred while executing '$program_path/$cp_command' command."
+        echo "An error occurred while executing '$thecase_path':"
+        echo "$output"
         exit 1
     fi
+}
+
+# The measurement of the energy consumption
+# e.g., ./test-configs/energy_measurement.sh ../pre-experiment/exe-ToyBox-v089 ./test-configs/wc-02.sh
+# basically, we call a specific test/example and send as argument the path of the program that we want it to run
+run_thecase_and_measure_energy() {
+    local thecase_path="$1"
+    local theprogram_path="$2"
+
+    sudo "$juleit" -n 1 "$thecase_path" "$theprogram_path"
+}
+
+main() {
+    # Checking the number of arguments
+    check_arguments "$@"
+
+    # Paths: 
+    # "../pre-experiment/exe-BusyBox-1360-final"
+    # "../pre-experiment/exe-ToyBox-v089"
+    # "./pre-experiment/exe-GNU-v93"
+    local program_path="../pre-experiment/exe-ToyBox-v089"
+    local the_case="$1"     # e.g., "./wc-02.sh"
+    local juleit="../src/jouleit.sh"
+
+    # Checking if inputs exists and are valid
+    validate_inputs "$the_case" "$program_path" "$juleit"
+
+    #echo "$program_path $the_case $juleit"
+    catch_errors_from_nested_script "$the_case" "$program_path"
+    
+    run_thecase_and_measure_energy "$the_case" "$program_path"
+
 }
 
 main "$@"
+
+# exit_code=$?
+# echo "$exit_code"
+# if [[ $exit_code -ne 0 ]]
+# then
+#     echo "An error occurred: $exit_code"
+# fi
