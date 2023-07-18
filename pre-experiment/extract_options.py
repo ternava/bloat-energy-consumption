c27362d0ca335265d90b7f8c14f78f9157e31a41
Lefeuvre Romain <romain.lefeuvre@univ-rennes1.fr>
2023-07-18 13:18:49 +0200

    ADD benchmark.csv, Commenting extract_options script

--- pre-experiment/extract_options.py
+++ pre-experiment/extract_options.py
@@ -1,4 +1,28 @@
 #Code genereated with the help of chatgpt
+###########################################
+#Description :
+
+# This script allow you to have an insight on the configuration option of an utility
+# It use the help page to pares with a regex the option
+# Actually it extract all the option of utilities contained in three folder and export the result in a csv file
+# You can customize the end of this script to extract the options of your folder 
+
+#Limitations :
+
+# The script do not handle properly option with a dash like "-aaaaa-bbbbb" it will return "-aaaaa"
+# Only the first option of each line is return (allow us to avoid many false positive match)
+# The option must be at the begining of line (it can have space or tab before)
+# It is not handling help template that have two different option on the same line (cf toybox ex)
+# Since it only keep one option per line you do not have both the reduce and long version of an option "-r vs --recursive"
+# We do not handle option that are not at the begining of a line (cf timeout of busybox "Usage: timeout [-s SIG] [-k KILL_SECS] SECS PROG ARGS")
+
+#Perspective :
+
+# We could use an LLM to perform such task
+# Customize this script to be 
+
+
+##########################################
 import os
 import subprocess
 import pandas as pd
@@ -6,27 +30,30 @@ import re
 def extract_options(folder_path):
     folder_name=os.path.basename(folder_path)
     utilities = []
-    options = []
+    all_options_list = []
 
     for filename in os.listdir(folder_path):
         filepath = os.path.join(folder_path, filename)
+        print(filename)
         if os.path.isfile(filepath) and os.access(filepath, os.X_OK):
             utility = os.path.splitext(filename)[0]
             utilities.append(utility)
             try:
                 result = subprocess.run([filepath, "--help"], capture_output=True, text=True)
-                if result.returncode == 0:
-                    option_list = extract_option_list(result.stdout)
-                    options.append(option_list)
-                else:
+                
+                option_list = extract_option_list(result.stdout)
+                
+                if result.returncode != 0 :
                     #ToyBox utilities have no --help option, the help page is displayed when you use a bad option
-                    print("Nothing on stdout, trying to find something on stderr,  for "+filepath)
-                    option_list = extract_option_list(result.stderr)
-                    options.append(option_list)
+                    #In some case the return code is 1/2 with help page in either stdout or stderr ...
+                    print("[WARN] Exit code not 0, trying to find something on stderr or stdout,  for "+filepath)
+                    stderr_option_list = extract_option_list(result.stderr)
+                    option_list += stderr_option_list                
+                all_options_list.append(option_list)
             except subprocess.CalledProcessError as e:
-                options.append([])
-    
-    df = pd.DataFrame({'Utility': utilities, 'Options '+folder_name: options})
+                all_options_list.append([])
+    option_size = [len(options_list) for options_list in all_options_list]
+    df = pd.DataFrame({'Utility': utilities, 'Options '+folder_name: all_options_list, 'Number of options'+folder_name: option_size})
     return df
 
 
@@ -56,9 +83,6 @@ for folder in folders:
     print(folder_df)
     result_df = folder_df if result_df is None else pd.merge(result_df, folder_df,on="Utility")
 
-result_df = result_df.rename(columns={'Options_x': 'Options_df1',
-                                      'Options_y': 'Options_df2',
-                                      'Options': 'Options_df3'})
 
 # Print the final DataFrame
 print(result_df)
