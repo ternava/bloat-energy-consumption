c02fda1135c2d307f1b5395f581e6af84d33016b
qperez <quentin.perez@inria.fr>
2023-07-20 11:35:18 +0200

    Merger for all the data in O1 and 02

--- notebooks/data_merger.py
+++ notebooks/data_merger.py
@@ -1,6 +1,26 @@
 import os
 import pandas as pd
 
+def merge_csv_files(directory_path, output_file):
+    # Get a list of all CSV files in the directory
+    csv_files = [file for file in os.listdir(directory_path) if file.endswith('.csv')]
+
+    # Initialize an empty list to hold DataFrames
+    dataframes = []
+
+    # Read each CSV file into a DataFrame and append it to the dataframes list
+    for file in csv_files:
+        file_path = os.path.join(directory_path, file)
+        df = pd.read_csv(file_path)
+        dataframes.append(df)
+
+    # Concatenate all DataFrames into a single DataFrame
+    merged_df = pd.concat(dataframes, ignore_index=True)
+
+    # Export the merged DataFrame to a new CSV file
+    merged_df.to_csv(output_file, index=False)
+    print("Merged CSV files have been exported to:", output_file)
+
 if __name__ == '__main__':
 
     directory = './data/repeat21'
@@ -53,15 +73,19 @@ if __name__ == '__main__':
                             data["PSYS_repeat" + column_name] = [df['PSYS'].values[0]]
                             data["DURATION_repeat" + column_name] = [df['DURATION'].values[0]]
                             data["Utilities"] = [utility]
-                            data["Command"] = [command]
+                            data["Program"] = [command]
+                            data["config"] = [config]
                         else:
                             data["PSYS_repeat" + column_name].append(df['PSYS'].values[0])
                             data["DURATION_repeat" + column_name].append(df['DURATION'].values[0])
                             data["Utilities"].append(utility)
-                            data["Command"].append(command)
-
+                            data["Program"].append(command)
+                            data["config"].append(config)
 
             # Create a new DataFrame using the data dictionary
             new_df = pd.DataFrame(data)
             # Write the new DataFrame to a CSV file
-            new_df.to_csv("./merged_data/" + command + "-" + config + '.csv', index=False)
+            new_df.to_csv("./merged_data_"+ config + "/"  + command + "-" + config + '.csv', index=False)
+
+    merge_csv_files("./merged_data_01", "./merged_data_01/all_data_01.csv")
+    merge_csv_files("./merged_data_02", "./merged_data_02/all_data_02.csv")
